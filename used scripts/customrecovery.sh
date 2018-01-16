b1016504993e6193be83a89e81e7a29b933f9e47
hvdwolf <hvdwolf@gmail.com>
2018-01-16 20:04:41 +0100

    upload scripts to give everyone insight

---
+++ used scripts/customrecovery.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+
+logFileName="/sdcard/JRI-$(date +'%Y%m%d').log"
+
+echo "$(date +'%d-%b-%Y %H:%M:%S') : joyingrecovery.sh script; write the custom partition." >> "${logFileName}"
+
+pimage=$(dd if=/sdcard/Download/recovery_new_keys.img of=/dev/block/mmcblk0p10  2>&1)
+
+echo "$(date +'%d-%b-%Y %H:%M:%S') : Output of the dd write process: $pimage" >> "${logFileName}"
+# And echo the outputstring as a double logging option. This output will be captured by the apk.
+echo "$pimage"
+
+
